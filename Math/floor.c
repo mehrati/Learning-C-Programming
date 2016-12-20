0d550e65d7992d462b2def7a1abd995905888243
mahdi <mahdi.robatipoor@gmail.com>
2016-12-20 13:03:31 +0330

    add file math

---
+++ Math/floor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float Value1, Value2, Value3, Value4;
+    Value1 = 1.6;
+    Value2 = 1.2;
+    Value3 = 2.8;
+    Value4 = 2.3;
+    printf("Value1 = %.1lf\n", floor(Value1));
+    printf("Value2 = %.1lf\n", floor(Value2));
+    printf("Value3 = %.1lf\n", floor(Value3));
+    printf("Value4 = %.1lf\n", floor(Value4));
+    return (0);
+}
